58875e56d1199493387ef0c904d56c7641be7624
Sameer Mirza <israfil334@gmail.com>
2020-04-27 18:58:05 -0700

    ?

--- src/App.js
+++ src/App.js
@@ -1,67 +1,57 @@
-import React, { createContext, useContext, useTransition } from "react";
+import React, { useTransition } from 'react';
+import store, { useStore } from '@jitcoder/usestore';
+
 import {
   syncBlock,
   useRegisterIncrementDispatcher,
+  reducer,
   ids,
-  useCheckTearing
-} from "./common";
-
-import store, { useStore } from "@jitcoder/usestore";
+  useCheckTearing,
+} from './common';
 
 const Counter = React.memo(() => {
+  const count = store.get('count', 0);
   syncBlock();
-  return <div className="count">{store.get("count")}</div>;
+  return <div className="count">{count}</div>;
 });
 
 const Main = () => {
-  const [count, setCount] = useStore("count");
-
+  const [count, setCount] = useStore('count', 0);
   useCheckTearing();
-  useRegisterIncrementDispatcher(
-    React.useCallback(() => {
-      setCount(count + 1);
-    }, [setCount])
-  );
-  const [localCount, localIncrement] = React.useReducer(c => c + 1, 0);
+  useRegisterIncrementDispatcher(React.useCallback(() => {
+    setCount(count+1);
+  }, [count]));
+
+  const [localCount] = useStore('count', 0);
+  const localIncrement = () => {
+    store.set('count', localCount + 1);
+  }
+
   const normalIncrement = () => {
-    setCount(count + 1);
+    store.set('count', count + 1);
   };
+
   const [startTransition, isPending] = useTransition();
   const transitionIncrement = () => {
     startTransition(() => {
-      setCount(count + 1);
+      store.set('count', count + 1);
     });
   };
-
   return (
     <div>
-      <button type="button" id="normalIncrement" onClick={normalIncrement}>
-        Increment shared count normally (two clicks to increment one)
-      </button>
-      <button
-        type="button"
-        id="transitionIncrement"
-        onClick={transitionIncrement}
-      >
-        Increment shared count in transition (two clicks to increment one)
-      </button>
-      <span id="pending">{isPending && "Pending..."}</span>
+      <button type="button" id="normalIncrement" onClick={normalIncrement}>Increment shared count normally (two clicks to increment one)</button>
+      <button type="button" id="transitionIncrement" onClick={transitionIncrement}>Increment shared count in transition (two clicks to increment one)</button>
+      <span id="pending">{isPending && 'Pending...'}</span>
       <h1>Shared Count</h1>
-      {ids.map(id => (
-        <Counter key={id} />
-      ))}
-      <div className="count">{store}</div>
+      {ids.map((id) => <Counter key={id} />)}
+      <div className="count">{count}</div>
       <h1>Local Count</h1>
       {localCount}
-      <button type="button" id="localIncrement" onClick={localIncrement}>
-        Increment local count
-      </button>
+      <button type="button" id="localIncrement" onClick={localIncrement}>Increment local count</button>
     </div>
   );
 };
 
-const App = () => {
-  return <Main />;
-};
+const App = () => <Main />;
 
 export default App;
